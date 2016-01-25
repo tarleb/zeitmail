a4c4dda67e86883da44c868dd694813775b792aa
Albert Krewinkel <albert@zeitkraut.de>
2016-01-25 23:00:22 +0100

    Add reverse DNS and testing host to test data
    Postfix has some checks involving reverse DNS, we need to account for
    those.

--- salt/roots/powerdns/data/test-data.sql
+++ salt/roots/powerdns/data/test-data.sql
@@ -1,50 +1,125 @@
 INSERT INTO domains (name, type) VALUES ('mail.test', 'NATIVE');
-INSERT INTO records (domain_id, name, type, content, ttl)
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
   VALUES (
     (SELECT id FROM domains WHERE name = 'mail.test'),
     'mail.test',
     'SOA',
     'ns1.mail.test hostmaster.mail.test',
-    86400
+    86400,
+    2016012501
   );
-INSERT INTO records (domain_id, name, type, content, ttl)
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
   VALUES (
     (SELECT id FROM domains WHERE name = 'mail.test'),
     'ns1.mail.test',
     'NS',
     '192.168.23.2',
-    3600
+    3600,
+    2016012501
   );
-INSERT INTO records (domain_id, name, type, content, ttl)
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
   VALUES (
     (SELECT id FROM domains WHERE name = 'mail.test'),
     'ns2.mail.test',
     'NS',
     '192.168.23.2',
-    3600
+    3600,
+    2016012501
   );
-INSERT INTO records (domain_id, name, type, content, ttl)
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
   VALUES (
     (SELECT id FROM domains WHERE name = 'mail.test'),
     'mail.test',
     'A',
     '192.168.23.2',
-    3600
+    3600,
+    2016012501
   );
-INSERT INTO records (domain_id, name, type, content, ttl)
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
   VALUES (
     (SELECT id FROM domains WHERE name = 'mail.test'),
     'smtp.mail.test',
     'A',
     '192.168.23.2',
-    3600
+    3600,
+    2016012501
   );
-INSERT INTO records (domain_id, name, type, content, ttl, prio)
+INSERT INTO records (domain_id, name, type, content, ttl, prio, change_date)
   VALUES (
     (SELECT id FROM domains WHERE name = 'mail.test'),
     'mail.test',
     'MX',
     'smtp.mail.test',
     3600,
-    10
+    10,
+    2016012501
+  );
+
+-- Testing server
+INSERT INTO domains (name, type) VALUES ('testing.test', 'NATIVE');
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
+  VALUES (
+    (SELECT id FROM domains WHERE name = 'testing.test'),
+    'testing.test',
+    'SOA',
+    'ns1.mail.test hostmaster.mail.test',
+    86400,
+    2016012501
+  );
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
+  VALUES (
+    (SELECT id FROM domains WHERE name = 'testing.test'),
+    'testing.test',
+    'NS',
+    'ns1.mail.test',
+    86400,
+    2016012501
+  );
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
+  VALUES (
+    (SELECT id FROM domains WHERE name = 'testing.test'),
+    'testing.test',
+    'A',
+    '192.168.23.42',
+    3600,
+    2016012501
+  );
+
+-- Setup reverse DNS
+INSERT INTO domains (name, type) VALUES ('23.168.192.in-addr.arpa', 'NATIVE');
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
+  VALUES (
+    (SELECT id FROM domains WHERE name = '23.168.192.in-addr.arpa'),
+    '23.168.192.in-addr.arpa',
+    'SOA',
+    'ns1.mail.test hostmaster.mail.test',
+    86400,
+    2016012501
+  );
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
+  VALUES (
+    (SELECT id FROM domains WHERE name = '23.168.192.in-addr.arpa'),
+    '23.168.192.in-addr.arpa',
+    'NS',
+    'ns1.mail.test',
+    86400,
+    2016012501
+  );
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
+  VALUES (
+    (SELECT id FROM domains WHERE name = '23.168.192.in-addr.arpa'),
+    '2.23.168.192.in-addr.arpa',
+    'PTR',
+    'mail.test',
+    86400,
+    2016012501
+  );
+INSERT INTO records (domain_id, name, type, content, ttl, change_date)
+  VALUES (
+    (SELECT id FROM domains WHERE name = '23.168.192.in-addr.arpa'),
+    '42.23.168.192.in-addr.arpa',
+    'PTR',
+    'testing.test',
+    86400,
+    2016012501
   );
