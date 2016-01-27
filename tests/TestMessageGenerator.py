c2bc0a100aa38960d981a8221114cab4a3bc96a6
Albert Krewinkel <albert@zeitkraut.de>
2016-01-27 12:18:02 +0100

    Allow to add a fake received header to test mails
    Spamassassin behaves differently if mail seems to originate from outside
    of the trusted (i.e. local) network.  A fake received header in the test
    message is enough to fool spamassassin to assume a different origin.
    Disable the header by default.

--- tests/TestMessageGenerator.py
+++ tests/TestMessageGenerator.py
@@ -11,16 +11,23 @@ class TestMessageGenerator:
             sender="sender@testing.test",
             recipient="root@mail.test",
             subject="zeitmail test",
-            gtube=False
+            gtube=False,
+            fakeReceived=False
         ):
         self.sender = sender
         self.recipient = recipient
         self.subject = subject
         self.gtube = gtube
+        self.fakeReceived=fakeReceived
 
     def generate_message(self):
         """Creates a new message using the current settings."""
         msg = email.message.EmailMessage()
+        if self.fakeReceived:
+            rcvd = "from funny.test (elite-mail.test [10.0.13.37]) by \
+              testing.test (Postfix) with ESMTPS for root@mail.test; \
+              Tue, 26 Jan 2016 09:36:49 +0000 (GMT)"
+            msg.add_header("Received", rcvd)
         msg["From"] = self.sender
         msg["To"] = self.recipient
         msg["Subject"] = "zeitmail test"
